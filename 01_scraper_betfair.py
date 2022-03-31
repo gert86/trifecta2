7b3fbe03d85328d30ba40fd4590a7fd7c71cccf7
Gert Hutter <gert.hutter@reactivereality.com>
2022-03-31 22:31:33 +0200

    Harmonized parameter dicts and outputs between all 3 scrapers
    Change-type: <major | minor | patch>
    Connects-to: #ISSUE
    Signed-off-by: Gert Hutter <gert.hutter@reactivereality.com>

--- 01_scraper_betfair.py
+++ 01_scraper_betfair.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import ElementClickInterceptedException
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pandas as pd
@@ -15,27 +16,29 @@ import re
 # PARAMS
 url = 'https://www.betfair.com/sport/football'
 outfile_name = './scraped/dict_betfair_test.pck'
-dict_countries = {
-               'german football': ['German Bundesliga', 'German Bundesliga 2'],
-        #   'italian football': ['Italian Serie A', 'Italian Serie B'],
-        #   'spanish football': ['Spanish La Liga', 'Spanish Segunda Division'],
-        #   'english football': ['English Premier League', 'English League 1', 'English League 2'],
-        #   'french football': ['French Ligue 1', 'French Ligue 2'],
-        #   'dutch football': ['Dutch Eredivisie'],
-        #   'belgian football': ['Belgian First Division A'],
-        #   'portuguese football': ['Portuguese Primeira Liga']
-        }                
-market_dict = {
-         'over2.5':'Over/Under 2.5 Goals', 
-         'btts':'Both teams to Score?'
-        }
+dict_leagues = {
+               'Germany Bundesliga'     : ('german football','German Bundesliga'),
+              # 'Germany 2. Bundesliga'  : ('german football', 'German Bundesliga 2'),
+              # 'Italy Serie A'          : ('italian football', 'Italian Serie A'),
+              # 'Italy Serie A'          : ('italian football', 'Italian Serie B'),
+              # 'Spain La Liga'          : ('spanish football', 'Spanish La Liga'),
+              # 'Spain Segunda Division' : ('spanish football', 'Spanish Segunda Division'),
+              # 'England Premier League' : ('english football', 'English Premier League'), 
+              # 'England League 1'       : ('english football', 'English League 1'),
+              # 'England League 2'       : ('english football', 'English League 2'),
+              # 'France Ligue 1'         : ('french football', 'French Ligue 1'),
+              # 'France Ligue 2'         : ('french football','French Ligue 2'),
+               } 
+              
+dict_markets =  {
+                '3way'    : 'Match Odds',  # needs special handling
+                'over2.5' : 'Over/Under 2.5 Goals', 
+                'btts'    : 'Both teams to Score?'
+                }
 
-# checks
-num_leagues = 0
-for country in dict_countries:
-  num_leagues = num_leagues + len(dict_countries[country])               
-if num_leagues==0 or len(market_dict)==0 :
-  print(f"Countries and Markets must both be non-empty!")
+# checks            
+if len(dict_leagues)==0 or len(dict_markets)==0 :
+  print(f"Leagues and Markets must both be non-empty!")
   exit(-1)
 
 # chromedriver options and init
@@ -47,8 +50,25 @@ driver.get(url)
 driver.maximize_window()
 
 # click accept cookies
+time.sleep(2)
 accept = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))         
 accept.click()
+
+# # click accept cookies
+# num_tries = 0
+# success = False
+# while num_tries < 3 and success == False:
+#   try:
+#     accept = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))         
+#     accept.click()
+#     success = True
+#   except:
+#     num_tries = num_tries + 1
+#     success = False
+#     print(f"Accepting cookies failed {num_tries} times")
+#     time.sleep(num_tries)
+
+
 
 # set website language to English and wait for reload
 language_box = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ssc-hlsw')))
@@ -62,86 +82,81 @@ count = 0
 today = datetime.datetime.today()
 tomorrow = today + datetime.timedelta(days=1)
 dict_frames = {} # 1 dataframe per league to be filled
-for country in dict_countries:
-  curr_loop_str = f"{country}"
-  try:
-    for league_idx in range(0, len(dict_countries[country])):        
-      count = count + 1
-      league = dict_countries[country][league_idx]
-      curr_loop_str = (f"{country}/{league}")
-      print(f"League {count} of {num_leagues}: {curr_loop_str}...")
+for league, league_data in dict_leagues.items():
+  count = count + 1
+  curr_loop_str = f"{league}"
+  print(f"League {count} of {len(dict_leagues)}: {curr_loop_str}...")
+  country, league_id = league_data
+  try:    
+    # click on competitions
+    header = driver.find_element(by=By.CLASS_NAME, value='updated-competitions')
+    competition = WebDriverWait(header, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@title, "COMPETITIONS")]')))
+    competition.click()
 
-      # click on competitions
-      header = driver.find_element(by=By.CLASS_NAME, value='updated-competitions')
-      competition = WebDriverWait(header, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@title, "COMPETITIONS")]')))
-      competition.click()
+    # click on countries button (e.g. "German Football")
+    competitions_table = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mod-multipickazmenu-1057')))
+    country_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/div[contains(@data-category,' +'"' + country + '"' + ')]')))
+    country_button.click() 
 
-      # click on countries button (e.g. "German Football")
-      competitions_table = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mod-multipickazmenu-1057')))
-      country_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/div[contains(@data-category,' +'"' + country + '"' + ')]')))
-      country_button.click() 
+    # click on country's league button (e.g. "German Bundesliga")
+    league_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@data-galabel,' +'"' + league_id + '"' + ')]')))
+    league_button.click()
+    print(f"  selected league {league}")            
 
-      # click on country's league button (e.g. "German Bundesliga")
-      league_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@data-galabel,' +'"' + dict_countries[country][league_idx] + '"' + ')]')))
-      league_button.click()
-      print(f"  selected league {league}")            
+    dict_odds = {}
+    for market, market_data in dict_markets.items():
+      curr_loop_str = (f"{country}/{league}/{market_data}")
+      list_dates = []
+      list_teams = []
+      list_odds = []
 
-      dict_odds = {}
-      for m_key, market in market_dict.items():
-        curr_loop_str = (f"{country}/{league}/{market}")
-        list_dates = []
-        list_teams = []
-        list_odds = []
-
-        
-        # click on dropdown box and select current market (e.g. "Over/Under 2.5 Goals")
+      # 3way has its own column without a drop down box, for others change the dropdown value
+      if market != '3way':        
         dropdown = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'marketchooser-container')))
         dropdown.click()
-        chooser = WebDriverWait(dropdown, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(),'+'"'+str(market)+'"'+')]')))
+        chooser = WebDriverWait(dropdown, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(),'+'"'+str(market_data)+'"'+')]')))
         chooser.click()
         time.sleep(1)  
-        print(f"      selected market {market}")        
 
-        games = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'event-information')))
-        for game in games:
-          date_time_str = game.find_element(by=By.XPATH, value='.//*[@class="date ui-countdown"]').text
-          list_dates.append(date_time_str)
+      games = WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'event-information')))              
+      for game in games:
+        date_time_str = game.find_element(by=By.XPATH, value='.//*[@class="date ui-countdown"]').text
+        list_dates.append(date_time_str)
 
-          odds = game.find_element(by=By.XPATH, value='.//div[contains(@class, "runner-list")]')
-          list_odds.append(odds.text)
-          
-          teams_container = game.find_element(by=By.CLASS_NAME, value='teams-container').text
-          list_teams.append(teams_container)
+        class_id = 'market-3-runners' if market=='3way' else 'market-2-runners'
+        odds = game.find_element(by=By.XPATH, value=f'.//div[@class="details-market {class_id}"]')
+        list_odds.append(odds.text)
         
-        #storing data dicts
-        dict_odds[f'dates_{m_key}'] = list_dates        
-        dict_odds[f'teams_{m_key}'] = list_teams
-        dict_odds[f'odds_{m_key}']  = list_odds
-        print(f"      added {len(list_dates)} games")
+        teams_container = game.find_element(by=By.CLASS_NAME, value='teams-container').text
+        list_teams.append(teams_container)
+      
+      #storing data dicts
+      dict_odds[f'dates_{market}'] = list_dates        
+      dict_odds[f'teams_{market}'] = list_teams
+      dict_odds[f'odds_{market}']  = list_odds
 
+    # concat markets and make 1 dataframe per league (still inside for loop)
+    df_list = []
+    for market in dict_markets.keys():
+      df_list.append(pd.DataFrame({'Dates':dict_odds[f'dates_{market}'], 'Teams':dict_odds[f'teams_{market}'], market:dict_odds[f'odds_{market}']}).set_index(['Teams', 'Dates'])) 
+    df_data = pd.concat(df_list, axis=1, sort=True)            
+    df_data.reset_index(inplace=True)
+    df_data.rename(columns={'index':'Teams'}, inplace=True)
 
-      # concat markets and make 1 dataframe per league (still inside for loop)
-      df_list = []
-      for m_key in market_dict.keys():
-        df_list.append(pd.DataFrame({'Dates':dict_odds[f'dates_{m_key}'], 'Teams':dict_odds[f'teams_{m_key}'], m_key:dict_odds[f'odds_{m_key}']}).set_index(['Teams', 'Dates'])) 
-      df_data = pd.concat(df_list, axis=1, sort=True)            
-      df_data.reset_index(inplace=True)
-      df_data.rename(columns={'index':'Teams'}, inplace=True)
+    # clean data
+    df_data = df_data.fillna('')
+    df_data = df_data.replace('SUSPENDED\n', '', regex=True)
+    df_data = df_data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
 
-      # clean data
-      df_data = df_data.fillna('')
-      df_data = df_data.replace('SUSPENDED\n', '', regex=True)
-      df_data = df_data.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    # replace words "In-Play", "Today" and "Tomorrow" with numeric dates
+    df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('In-Play', today.strftime("%A, %d %B"), x))
+    df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('Today', today.strftime("%A, %d %B"), x))
+    df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('Tomorrow', tomorrow.strftime("%d %b"), x))
+    df_data['Dates'] = df_data['Dates'].apply(lambda x: datetime.datetime.strptime(str(today.year) + ' ' + x, '%Y %d %b %H:%M'))
 
-      # replace words "In-Play", "Today" and "Tomorrow" with numeric dates
-      df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('In-Play', today.strftime("%A, %d %B"), x))
-      df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('Today', today.strftime("%A, %d %B"), x))
-      df_data['Dates'] = df_data['Dates'].apply(lambda x: re.sub('Tomorrow', tomorrow.strftime("%d %b"), x))
-      df_data['Dates'] = df_data['Dates'].apply(lambda x: datetime.datetime.strptime(str(today.year) + ' ' + x, '%Y %d %b %H:%M'))
-
-      #storing dataframe of each league in dictionary
-      dict_frames[dict_countries[country][league_idx]] = df_data
-      print(f"Finished {curr_loop_str}\n\n")
+    #storing dataframe of each league in dictionary
+    dict_frames[league] = df_data
+    print(f"Finished {curr_loop_str}\n\n")
   except Exception as e:
     print(f"\n\nException in {curr_loop_str}: {str(e)}\n\n")
     driver.quit()
@@ -154,13 +169,12 @@ for league, df in dict_frames.items():
   for idx, row in df.iterrows():
     home_team, away_team = str(row['Teams']).split('\n')
     print(f"    {row['Dates']}: {home_team} vs. {away_team}")
-    for curr_market in market_dict.keys():
-      odds = str(row[curr_market]).replace('\n', ' ')
-      print(f"      {curr_market}: {odds}")
+    for market in dict_markets.keys():
+      odds = str(row[market]).replace('\n', ' ')
+      print(f"      {market}: {odds}")
 
 #save file
 output = open(outfile_name, 'wb')
 pickle.dump(dict_frames, output)
 output.close()
 print(f"Done. Stored to {outfile_name}")
-
--- 02_scraper_tipico.py
+++ 02_scraper_tipico.py
@@ -15,29 +15,30 @@ import re
 # PARAMS
 url = 'https://sports.tipico.de/en/all/football/'
 outfile_name = './scraped/dict_tipico.pck'
-# Note: Tipico allows a filter for all these leagues in 1 page with a dedicated URL (would be lot faster) 
-dict_countries = {
-          'german football': ['germany/bundesliga', 'germany/2-bundesliga'],
-        #   'italian football': ['italy/serie-a', 'italy/serie-b'],
-        #   'spanish football': ['spain/la-liga', 'spain/la-liga-2'],
-        #   'english football': ['england/premier-league', 'england/league-one', 'england/league-two'],
-        #   'french football': ['france/ligue-1', 'france/ligue-2'],
-        #   'dutch football': ['netherlands/eredivisie'],
-        #   'belgian football': ['belgium/first-division-a'],
-        #   'portuguese football': ['portugal/primeira-liga']
-        }                      
-market_dict = {
-         'over_under':'Over/Under', 
-         'btts'      :'Both Teams to Score',
-         '3way'      :'3-Way'
-         }
+# Note: Tipico allows a filter for all these leagues in 1 page with a dedicated URL (would be lot faster)
+dict_leagues = {
+               'Germany Bundesliga'     : 'germany/bundesliga',
+              # 'Germany 2. Bundesliga'  :'germany/2-bundesliga',
+               'Italy Serie A'          : 'italy/serie-a',
+              # 'Italy Serie A'          : 'italy/serie-b',
+              # 'Spain La Liga'          : 'spain/la-liga',
+              # 'Spain Segunda Division' : 'spain/la-liga-2',
+              # 'England Premier League' : 'england/premier-league', 
+              # 'England League 1'       : 'england/league-one',
+              # 'England League 2'       : 'england/league-two',
+               'France Ligue 1'         : 'france/ligue-1',
+              # 'France Ligue 2'         : 'france/ligue-2',
+               }    
+    
+dict_markets =  {
+                '3way'    : '3-Way',
+                'over2.5' : 'Over/Under', 
+                'btts'    : 'Both Teams to Score'
+                }         
 
-# checks
-num_leagues = 0
-for country in dict_countries:
-  num_leagues = num_leagues + len(dict_countries[country])               
-if num_leagues==0 or len(market_dict)==0 :
-  print(f"Countries and Markets must both be non-empty!")
+# checks            
+if len(dict_leagues)==0 or len(dict_markets)==0 :
+  print(f"Leagues and Markets must both be non-empty!")
   exit(-1)
 
 # chromedriver options and init
@@ -56,95 +57,91 @@ accept.click()
 #loop through leagues
 count = 0
 dict_frames = {} # 1 dataframe per league to be filled
-for country in dict_countries:
-  curr_loop_str = f"{country}"
+for league, league_data in dict_leagues.items():
+  count = count + 1
+  curr_loop_str = f"{league}"
+  print(f"League {count} of {len(dict_leagues)}: {curr_loop_str}...")
+  league_url_suffix = league_data
   try:        
-    for league_idx in range(0, len(dict_countries[country])):
-      dict_odds = {}  
-      count = count + 1
-      league = dict_countries[country][league_idx]
-      curr_loop_str = (f"{country}/{league}")
-      print(f"League {count} of {num_leagues}: {curr_loop_str}...")
-
-      # navigate to subpage
-      full_url = url + league
-      driver.get(full_url)
-      time.sleep(2)
-          
-      # TODO: Currently not changing dropdown boxes
-      # Use the following code to change dd values (BEFORE getting dropdowns)
-      # Current problem: Site does not allow the same selected value in different dds
-      #first_dropdown = Select(dropdowns[0])
-      #first_dropdown.select_by_visible_text(market)            
-      dropdowns = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'SportHeader-styles-drop-down')))
-      dropdown_markets = []
-      for i in range(0, len(dropdowns)):
-        dropdown_markets.append(Select(dropdowns[i]).first_selected_option.accessible_name)
-      if not set(market_dict.values()).issubset(set(dropdown_markets)):
-        print(f"Not all configured markets {market_dict.values()} are visible: {dropdown_markets}")
-        exit(-1)    
+    # navigate to subpage
+    full_url = url + league_url_suffix
+    driver.get(full_url)
+    time.sleep(2)
+        
+    # TODO: Currently not changing dropdown boxes
+    # Use the following code to change dd values (BEFORE getting dropdowns)
+    # Current problem: Site does not allow the same selected value in different dds
+    #first_dropdown = Select(dropdowns[0])
+    #first_dropdown.select_by_visible_text(market)            
+    dropdowns = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'SportHeader-styles-drop-down')))
+    dropdown_markets = []
+    for i in range(0, len(dropdowns)):
+      dropdown_markets.append(Select(dropdowns[i]).first_selected_option.accessible_name)
+    if not set(dict_markets.values()).issubset(set(dropdown_markets)):
+      print(f"Not all configured markets {dict_markets.values()} are visible: {dropdown_markets}")
+      exit(-1)    
 
           
-      # get table and immediate children    
-      table = driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/main/section/div/div[1]/div/div/div')            
-      children=table.find_elements(by=By.XPATH,value='./*')
+    # get table and immediate children    
+    table = driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/main/section/div/div[1]/div/div/div')            
+    children=table.find_elements(by=By.XPATH,value='./*')
 
-      # children are a mix of dates (<div>) and cells containing game data (<a>). 
-      # Need to be processed in order
-      games_with_dates = []
-      curr_date = ''
-      for child in children:
-        if child.tag_name == 'div':
-          try:
-            if ',' in child.text:
-              date_time_obj = datetime.datetime.strptime(child.text.split(',')[1].strip(), '%d.%m').replace(year=datetime.datetime.now().year)
-              curr_date = date_time_obj.strftime('%Y-%m-%d')
-          except:
-            pass
-        if child.tag_name == 'a':
-          games_with_dates.append((child, curr_date))
-      print(f"  there are {len(games_with_dates)} games ...")
+    # children are a mix of dates (<div>) and cells containing game data (<a>). 
+    # Need to be processed in order
+    games_with_dates = []
+    curr_date = ''
+    for child in children:
+      if child.tag_name == 'div':
+        try:
+          if ',' in child.text:
+            date_time_obj = datetime.datetime.strptime(child.text.split(',')[1].strip(), '%d.%m').replace(year=datetime.datetime.now().year)
+            curr_date = date_time_obj.strftime('%Y-%m-%d')
+        except:
+          pass
+      if child.tag_name == 'a':
+        games_with_dates.append((child, curr_date))
 
-      # that would be the easy way, if we would not need the dates
-      #games = table.find_elements(by=By.XPATH,value='./a[@class="EventRow-styles-event-row"]')
-      
-      for game,date_str in games_with_dates:
-        child_divs=game.find_elements(by=By.XPATH,value='./div')
-        if len(child_divs) < 3:
+    # that would be the easy way, if we would not need the dates
+    #games = table.find_elements(by=By.XPATH,value='./a[@class="EventRow-styles-event-row"]')
+    
+    for game,date_str in games_with_dates:
+      child_divs=game.find_elements(by=By.XPATH,value='./div')
+      if len(child_divs) < 3:
+        continue
+      if len(child_divs[0].find_elements(by=By.XPATH, value='.//*[@class="EventDateTime-styles-time EventDateTime-styles-no-date"]')) != 1:
+        continue
+      if len(child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')) != 2:
+        continue
+
+      start_time_str = child_divs[0].find_element(by=By.XPATH, value='.//*[@class="EventDateTime-styles-time EventDateTime-styles-no-date"]').text
+      home_team = child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')[0].text
+      away_team = child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')[1].text
+      print(f"    {date_str} {start_time_str}: {home_team} vs. {away_team}")
+
+      for i in range(2, len(child_divs)):
+        odd_buttons = child_divs[i].find_elements(by=By.XPATH,value='.//button')
+        num_odd_buttons = len(odd_buttons)
+        curr_market_dd = dropdown_markets[i-2] if i-2 < len(dropdown_markets) else ''
+        if num_odd_buttons < 2 or num_odd_buttons > 3 or curr_market_dd not in dict_markets.values():
           continue
-        if len(child_divs[0].find_elements(by=By.XPATH, value='.//*[@class="EventDateTime-styles-time EventDateTime-styles-no-date"]')) != 1:
-          continue
-        if len(child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')) != 2:
-          continue
-
-        start_time_str = child_divs[0].find_element(by=By.XPATH, value='.//*[@class="EventDateTime-styles-time EventDateTime-styles-no-date"]').text
-        home_team = child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')[0].text
-        away_team = child_divs[1].find_elements(by=By.XPATH,value='.//*[@class="EventTeams-styles-team-title"]')[1].text
-        print(f"    {date_str} {start_time_str}: {home_team} vs. {away_team}")
-
-        for i in range(2, len(child_divs)):
-          odd_buttons = child_divs[i].find_elements(by=By.XPATH,value='.//button')
-          num_odd_buttons = len(odd_buttons)
-          curr_market = dropdown_markets[i-2] if i-2 < len(dropdown_markets) else ''
-          if num_odd_buttons < 2 or num_odd_buttons > 3 or curr_market not in market_dict.values():
-            continue
-          
-          if num_odd_buttons == 3:
-            odd_home, odd_draw, odd_away = odd_buttons[0].text, odd_buttons[1].text, odd_buttons[2].text
-            print(f"      {curr_market}: {odd_home}, {odd_draw}, {odd_away}")
-          elif num_odd_buttons == 2:
-            odd_home, odd_away = odd_buttons[0].text, odd_buttons[1].text
-            print(f"      {curr_market}: {odd_home}, {odd_away}")    
-          else:      
-            print(f"      Found no odds for {curr_market}")                        
+                
+        market = list(dict_markets.keys())[list(dict_markets.values()).index(curr_market_dd)] # key from value
+        if num_odd_buttons == 3:
+          odd_home, odd_draw, odd_away = odd_buttons[0].text, odd_buttons[1].text, odd_buttons[2].text
+          print(f"      {market}: {odd_home}, {odd_draw}, {odd_away}")
+        elif num_odd_buttons == 2:
+          odd_home, odd_away = odd_buttons[0].text, odd_buttons[1].text
+          print(f"      {market}: {odd_home}, {odd_away}")    
+        else:      
+          print(f"      Found no odds for {market}")                        
           
       
       continue                     
 
       # concat markets and make 1 dataframe per league (still inside for loop)
       df_list = []
-      for m_key in market_dict.keys():
-        df_list.append(pd.DataFrame({'Dates':dict_odds[f'dates_{m_key}'], 'Teams':dict_odds[f'teams_{m_key}'], m_key:dict_odds[f'odds_{m_key}']}).set_index(['Teams', 'Dates'])) 
+      for market in dict_markets.keys():
+        df_list.append(pd.DataFrame({'Dates':dict_odds[f'dates_{market}'], 'Teams':dict_odds[f'teams_{market}'], market:dict_odds[f'odds_{market}']}).set_index(['Teams', 'Dates'])) 
       df_data = pd.concat(df_list, axis=1, sort=True)            
       df_data.reset_index(inplace=True)
       df_data.rename(columns={'index':'Teams'}, inplace=True)
@@ -164,7 +161,7 @@ for country in dict_countries:
       df_data['Dates'] = df_data['Dates'].apply(lambda x: datetime.datetime.strptime(str(today.year) + ' ' + x, '%Y %d %B'))
 
       #storing dataframe of each league in dictionary
-      dict_frames[dict_countries[country][league_idx]] = df_data
+      dict_frames[league] = df_data
       print(f"Finished {curr_loop_str}\n\n")
   except Exception as e:
     print(f"\n\nException in {curr_loop_str}: {str(e)}\n\n")
@@ -178,5 +175,3 @@ output = open(outfile_name, 'wb')
 pickle.dump(dict_frames, output)
 output.close()
 print(f"Done. Stored to {outfile_name}")
-
-# WORKS ALMOST
