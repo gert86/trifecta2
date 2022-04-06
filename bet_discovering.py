6943aba2930e4a3707eca8448b810a8e5f594769
Gert Hutter <gert.hutter@reactivereality.com>
2022-04-06 17:08:29 +0200

    Made bet_discovering.py generic for all markets
    Change-type: <major | minor | patch>
    Connects-to: #ISSUE
    Signed-off-by: Gert Hutter <gert.hutter@reactivereality.com>

--- bet_discovering.py
+++ bet_discovering.py
@@ -18,6 +18,9 @@ def score(vals):
   val = 1.0/((1.0/np.asarray(vals)).sum())
   return f"{val:.4f}"
 
+def joinEm(vals):
+  return ' / '.join([str(x) for x in vals]) 
+
 def printHistogram(scores, bookie_list, market):
   if not scores:
     return
@@ -31,6 +34,63 @@ def printHistogram(scores, bookie_list, market):
   plt.show()
 
 
+def findBets(unified_dicts, market):  
+  # only iterate those dicts for which we have the respective data
+  eligible_dicts = {}
+  for bookie, unified_dict in unified_dicts.items(): 
+    if market in list(unified_dict[ list(unified_dict.keys())[0] ].columns):
+      eligible_dicts[bookie] = unified_dict
+  print(f"The following bookies have data for {market}: {eligible_dicts.keys()}")
+
+  # find best odds
+  best_odds_data = {}
+  for bookie, unified_dict in eligible_dicts.items():  
+    for league, df in unified_dict.items():
+      for index,row in df.iterrows():
+        if not index or not row[market]:
+          continue
+        date, home_team, away_team = index
+        odds = [float(x) for x in row[market].split('\n')]
+        if not index in best_odds_data.keys():
+          #print(f"Found new game: {date}: {home_team} vs {away_team} -> Odds: {joinEm(odds)}. Score: { score(odds) }")
+          best_odds_data[index] = [odds, [bookie]*len(odds)]
+        else:
+          #print(f"Game already exists: {date}: {home_team} vs {away_team}")      
+          old_odds = best_odds_data[index][0]
+          old_bookies = best_odds_data[index][1]
+          old_score = score(old_odds)
+          improved = False
+          for i in range(len(odds)):
+            if best_odds_data[index][0][i] < odds[i]:
+              best_odds_data[index][0][i] = odds[i]
+              best_odds_data[index][1][i] = bookie
+              improved = True
+          # if improved:
+          #   new_odds = best_odds_data[index][0]
+          #   new_score = score(new_odds)
+          #   print(f"  Improved Odds: {old_odds} --> {new_odds}. Improved score: {old_score} --> {new_score}")
+
+  # verify best odds
+  surebets = {}
+  best_scores = {}
+  for index, odds_data in best_odds_data.items():
+    date, home_team, away_team = index
+    odds = odds_data[0]
+    bookies = odds_data[1]
+    curr_score = float(score(odds))
+    best_scores[index] = curr_score
+    #print(f"Best score {home_team} vs. {away_team}: {curr_score} -> {joinEm(bookies)")
+    if curr_score > 1.0:
+      print(f"Surebet: {date.date()}: {home_team} vs {away_team} " + \
+            f"-> Odds: {joinEm(odds)} " + \
+            f"-> Bookies: {joinEm(bookies)} (score: {curr_score})")
+      surebets[index] = odds_data
+
+  # print stats
+  print(f"Done. Found {len(surebets)} surebets for {market}.")
+  printHistogram(best_scores, bookies, market)
+
+
 ###########################################################################################################
 #  MAIN
 
@@ -41,137 +101,6 @@ for bookie in bookies:
   print(f"{bookie}: Length: {len(unified_dict)}. Keys: {unified_dict.keys()}")
   unified_dicts[bookie] = unified_dict
 
-
-
-
-# only iterate those dicts for which we have 3-way data
-market = '3-way'
-eligible_dicts = {}
-for bookie, unified_dict in unified_dicts.items(): 
-  if market in list(unified_dict[ list(unified_dict.keys())[0] ].columns):
-    eligible_dicts[bookie] = unified_dict
-print(f"The following bookies have data for {market}: {eligible_dicts.keys()}")
-
-
-# find best odds 3-way
-best_odds = {}
-for bookie, unified_dict in eligible_dicts.items():  
-  for league, df in unified_dict.items():
-    for index,row in df.iterrows():
-      if not index or not row[market]:
-        continue
-      date, home_team, away_team = index
-      odd_1, odd_X, odd_2 = [float(x) for x in row[market].split('\n')]
-      if not index in best_odds.keys():
-        #print(f"Found new game: {date}: {home_team} vs {away_team} -> Odds: {odd_1} / {odd_X} / {odd_2}. Score: { score([odd_1,odd_X,odd_2]) }")
-        best_odds[index] = ([odd_1, odd_X, odd_2], [bookie, bookie, bookie])
-      else:
-        #print(f"Game already exists: {date}: {home_team} vs {away_team}")      
-        old_odds = best_odds[index][0]
-        old_bookies = best_odds[index][1]
-        old_score = score(old_odds)
-        improved = False
-
-        if best_odds[index][0][0] < odd_1:
-          best_odds[index] = ((odd_1, best_odds[index][0][1], best_odds[index][0][2]),    (bookie, best_odds[index][1][1], best_odds[index][1][2]))
-          improved = True
-        if best_odds[index][0][1] < odd_X:
-          best_odds[index] = ((best_odds[index][0][0], odd_X, best_odds[index][0][2]),    (best_odds[index][1][0], bookie, best_odds[index][1][2]))
-          improved = True
-        if best_odds[index][0][2] < odd_2:
-          best_odds[index] = ((best_odds[index][0][0], best_odds[index][0][1], odd_2),    (best_odds[index][1][0], best_odds[index][1][1], bookie))
-          improved = True
-
-        if improved:
-          new_odds = best_odds[index][0]
-          new_score = score(new_odds)
-          #print(f"  Improved Odds: {old_odds} --> {new_odds}. Improved score: {old_score} --> {new_score}")
-
-# verify best odds 3-way 
-surebets = {}
-best_scores = {}
-for index, odds in best_odds.items():
-  date, home_team, away_team = index
-  odd_1, odd_X, odd_2 = odds[0]
-  bookie_1, bookie_X, bookie_2 = odds[1]
-  curr_score = float(score([odd_1, odd_X, odd_2]))
-  best_scores[index] = curr_score
-  #print(f"Best score {home_team} vs. {away_team}: {curr_score} -> {bookie_1}/{bookie_X}/{bookie_2}")
-  if curr_score > 1.0:
-    print(f"Surebet: {date.date()}: {home_team} vs {away_team} " + \
-          f"-> Odds: {odd_1} / {odd_X} / {odd_2} " + \
-          f"-> Bookies: {bookie_1} / {bookie_X} / {bookie_2} (score: { curr_score })")
-    surebets[index] = odds
-
-# print stats 3-way
-print(f"Done. Found {len(surebets)} surebets for {market}.")
-printHistogram(best_scores, bookies, market)
-
-
-
-
-
-
-# only iterate those dicts for which we have btts data
-market = 'btts'
-eligible_dicts = {}
-for bookie, unified_dict in unified_dicts.items(): 
-  if market in list(unified_dict[ list(unified_dict.keys())[0] ].columns):
-    eligible_dicts[bookie] = unified_dict
-print(f"The following bookies have data for {market}: {eligible_dicts.keys()}")
-
-
-# find best odds btts
-best_odds = {}
-for bookie, unified_dict in eligible_dicts.items():  
-  for league, df in unified_dict.items():
-    for index,row in df.iterrows():
-      if not index or not row[market]:
-        continue
-      date, home_team, away_team = index
-      odd_1, odd_2 = [float(x) for x in row[market].split('\n')]
-      if not index in best_odds.keys():
-        #print(f"Found new game: {date}: {home_team} vs {away_team} -> Odds: {odd_1} / {odd_2}. Score: { score([odd_1,odd_2]) }")
-        best_odds[index] = ((odd_1, odd_2), (bookie, bookie))
-      else:
-        #print(f"Game already exists: {date}: {home_team} vs {away_team}")      
-        old_odds = best_odds[index][0]
-        old_bookies = best_odds[index][1]
-        old_score = score(old_odds)
-        improved = False
-
-        if best_odds[index][0][0] < odd_1:
-          best_odds[index] = ((odd_1, best_odds[index][0][1]),    (bookie, best_odds[index][1][1]))
-          improved = True
-        if best_odds[index][0][1] < odd_2:
-          best_odds[index] = ((best_odds[index][0][0], odd_2),    (best_odds[index][1][0], bookie))
-          improved = True
-
-        if improved:
-          new_odds = best_odds[index][0]
-          new_score = score(new_odds)
-          #print(f"  Improved Odds: {old_odds} --> {new_odds}. Improved score: {old_score} --> {new_score}")
-
-# verify best odds btts 
-surebets = {}
-best_scores = {}
-for index, odds in best_odds.items():
-  date, home_team, away_team = index
-  odd_1, odd_2 = odds[0]
-  bookie_1, bookie_2 = odds[1]
-  curr_score = float(score([odd_1, odd_2]))
-  best_scores[index] = curr_score
-  #print(f"Best score {home_team} vs. {away_team}: {curr_score} -> {bookie_1}/{bookie_2}")
-  if curr_score > 1.0:
-    print(f"Surebet: {date.date()}: {home_team} vs {away_team} " + \
-          f"-> Odds: {odd_1} / {odd_2} " + \
-          f"-> Bookies: {bookie_1} / {bookie_2} (score: { curr_score })")
-    surebets[index] = odds
-
-# print stats btts
-print(f"Done. Found {len(surebets)} surebets for {market}.")
-printHistogram(best_scores, bookies, market)
-
-
-
+findBets(unified_dicts, '3-way')
+findBets(unified_dicts, 'btts')
 
